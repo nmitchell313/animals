9e2fab81f4e26ad8572c545d9462b3d253784b5d
nmitchell313 <nmitchel11@cnm.edu>
2021-10-01 12:03:56 -0600

    added model & web service proxy

---
+++ app/src/main/java/edu/cnm/deepdive/animals/model/Animal.java
@@ -0,0 +1,41 @@
+package edu.cnm.deepdive.animals.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Animal {
+
+    @Expose
+    private String title;
+
+    @Expose
+    private String description;
+
+    @Expose
+    @SerializedName("href")
+    private String imageUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
