49151b83f64de2a5acd8d1a6f87b979364de2fb7
nmitchell313 <nmitchel11@cnm.edu>
2021-10-05 11:06:25 -0600

    ADDED ANDROID MANIFEST AND MAIN ACTIVITY.

--- app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
@@ -1,7 +1,14 @@
 package edu.cnm.deepdive.animals;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import edu.cnm.deepdive.animals.model.Animal;
+import edu.cnm.deepdive.animals.service.WebServiceProxy;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Retriever().start();
+    }
+
+    private class Retriever extends Thread {
+
+        @Override
+        public void run() {
+
+            try {
+                Response<List<Animal>> response = WebServiceProxy.getInstance()
+                        .getAnimals()
+                        .execute();
+                if (response.isSuccessful()) {
+                    Log.d(getClass().getName(), response.body().toString());
+                } else {
+                  Log.e(getClass().getName(), response.message());
+                }
+            } catch (IOException e) {
+                Log.e(getClass().getName(), e.getMessage());
+            }
+        }
     }
 }
